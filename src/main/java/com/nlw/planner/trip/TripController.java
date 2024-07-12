31de9415e069f6423ffc68f30c488b70ea235d2a
Everton Rocha <imevertonrch@gmail.com>
2024-07-11 23:08:52 -0300

    put mapping

--- src/main/java/com/nlw/planner/trip/TripController.java
+++ src/main/java/com/nlw/planner/trip/TripController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,13 +28,38 @@ public class TripController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Trip> getTrip(@PathVariable("id") UUID tripId) {
+    public ResponseEntity<Trip> getTripDetails(@PathVariable("id") UUID tripId) {
         Optional<Trip> tripNullable = this.repository.findById(tripId);
 
         return tripNullable.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
-    
+
+    @PutMapping("/{tripId}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable UUID tripId,
+                                           @RequestBody TripRequest request) {
+        
+        Optional<Trip> tripNullable = this.repository.findById(tripId);
+
+        if(tripNullable.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        System.out.println(request);
+
+        Trip updatedTrip = tripNullable.get();
+        updatedTrip.setDestination(request.destination());
+        updatedTrip.setStartsAt(parseToLocalDateTime(request.startsAt()));
+        updatedTrip.setEndsAt(parseToLocalDateTime(request.endsAt()));
+        this.repository.save(updatedTrip);
+
+        return ResponseEntity.ok(updatedTrip);
+    }
+
+    private LocalDateTime parseToLocalDateTime(String textDate) {
+        return LocalDateTime.parse(textDate, DateTimeFormatter.ISO_DATE_TIME);
+    }
+
     private URI createUri(UUID id, UriComponentsBuilder uriBuilder) {
         return uriBuilder.path("/trips/{tripId}")
                 .buildAndExpand(id)
